88dcfd650e9a17bb8c58b213045e7e694e369fbc
huyenanh3010 <nguyenhuyenanhac2025@gmail.com>
2026-02-05 17:04:22 +0700

    demopart4

--- gamedemo/Assets/Scripts/Bullet.cs
+++ gamedemo/Assets/Scripts/Bullet.cs
@@ -3,8 +3,16 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float flySpeed;
-
-    // Update is called once per frame
+    public int damage;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var enemy = collision.GetComponent<EnemyHealth>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+        Destroy(gameObject);
+    }
     void Update()
     {
         var newPosition = transform.position;
